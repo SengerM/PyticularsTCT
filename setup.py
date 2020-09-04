11096b69073c0842664bf230cbdc164d91a5a4fb
Matias Senger <m.senger@hotmail.com>
2020-09-04 14:45:43 +0200

    asd

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 datadir = Path(__file__)/'PyticularsTCT'/'ximc'
 files = [str(p.relative_to(datadir)) for p in datadir.rglob('*.dat')]
+
+print(files)
+
 # with open("README.md", "r") as fh:
 	# long_description = fh.read()
 
