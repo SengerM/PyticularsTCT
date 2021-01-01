e36905ba81e2a7271e265bc80c8371ca7d5eda14
Matias Senger <m.senger@hotmail.com>
2021-01-01 13:54:45 +0100

    New architecture of script with function

--- scripts/scan_xy_4_channels.py
+++ scripts/scan_xy_4_channels.py
@@ -3,48 +3,33 @@ from PyticularsTCT.tct_setup import TCTStages # https://github.com/SengerM/Pytic
 from PyticularsTCT.utils import save_4ch_trigger # https://github.com/SengerM/PyticularsTCT
 import numpy as np
 from time import sleep
-import os
 import myplotlib as mpl
-from PyticularsTCT.utils import read_4ch_trigger
 from lgadtools.LGADSignal import LGADSignal # https://github.com/SengerM/lgadtools
-from myplotlib.utils import get_timestamp
-import random
-from bureaucrat import Bureaucrat
+from data_processing_bureaucrat.Bureaucrat import Bureaucrat # https://github.com/SengerM/data_processing_bureaucrat
+from pathlib import Path
 
-############################################################
-
-# (0.021295419921875, 0.038191191406250005, 0.056478310546875)
-
-X_START = 21.14e-3
-X_END = 21.32e-3
-Y_START = 37.28e-3
-Y_END = 37.51e-3
-N_STEPS = 99
-Z_FOCUS = 54.8e-3
-N_AVERAGE_TRIGGERS = 55
-
-############################################################
-
-bureaucrat = Bureaucrat(
-	measurement_name = str(input('Measurement name? ')).replace(' ', '_')
-)
-print(f'Data will be saved in {bureaucrat.measurement_path}')
-
-def measure():
+def script_core(measurement_name: str, x_start: float, x_end: float, y_start: float, y_end: float, z_focus: float, n_steps: int, n_average_triggers: int = 1):
+	for var in [x_start, x_end, y_start, y_end, z_focus]:
+		if var > 1: 
+			raise ValueError(f'Check the values of x_start, x_end, y_start, y_end and z_focus. One of them is {var} which is more than one meter, this has to be wrong.')
+	bureaucrat = Bureaucrat(
+		str(Path(f'C:/Users/tct_cms/Desktop/TCT_measurements_data/{measurement_name}')),
+		variables = locals(),
+		new_measurement = True,
+	)
+	
 	osc = LecroyWR640Zi('USB0::0x05FF::0x1023::4751N40408::INSTR')
 	stages = TCTStages()
 
 	print('Moving to start position...')
 	stages.move_to(
-		x = X_START,
-		y = Y_START,
-		z = Z_FOCUS
+		x = x_start,
+		y = y_start,
+		z = z_focus,
 	)
-	print(f'Current position is {stages.position} m')
-	print('Measuring...')
 	
-	for nx,x_position in enumerate(np.linspace(X_START,X_END,N_STEPS)):
-		for ny,y_position in enumerate(np.linspace(Y_START,Y_END,N_STEPS)):
+	for nx,x_position in enumerate(np.linspace(x_start,x_end,n_steps)):
+		for ny,y_position in enumerate(np.linspace(y_start,y_end,n_steps)):
 			print('#############################')
 			print(f'nx, ny = {nx}, {ny}')
 			print(f'Moving stages to {(x_position,y_position)} m...')
@@ -56,7 +41,7 @@ def measure():
 			print('Acquiring signals...')
 			sleep(0.01)
 			averaged_signals = {}
-			for n_average in range(N_AVERAGE_TRIGGERS):
+			for n_average in range(n_average_triggers):
 				signals = osc.acquire_one_pulse()
 				if n_average == 0:
 					for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
@@ -66,7 +51,7 @@ def measure():
 					for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
 						averaged_signals[ch]['volt'] += np.array(signals[ch]['volt'])
 			for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
-				averaged_signals[ch]['volt'] /= N_AVERAGE_TRIGGERS
+				averaged_signals[ch]['volt'] /= n_average_triggers
 			
 			fname = f'{bureaucrat.raw_data_dir_path}/{nx:05d}-{ny:05d}.txt'
 			print(f'Saving data in "{fname}"...')
@@ -80,7 +65,7 @@ def measure():
 				subtitle = f'Measurement {bureaucrat.measurement_name}',
 				xlabel = 'Time (s)',
 				ylabel = 'Amplitude (V)',
-				package = 'plotly' if np.random.rand() < 20/N_STEPS**2 else 'matplotlib',
+				package = 'plotly' if np.random.rand() < 20/n_steps**2 else 'matplotlib',
 			)
 			for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
 				temp_fig.plot(
@@ -90,74 +75,35 @@ def measure():
 				)
 			mpl.manager.save_all(mkdir=f'{bureaucrat.processed_data_dir_path}/raw_signals_plots')
 			mpl.manager.delete_all_figs()
-			
+				
 	print('Finished measuring! :)')
-
-def parse_amplitudes():
-	print('Reading data...')
-	raw_data = []
-	for fname in sorted(os.listdir(bureaucrat.raw_data_dir_path)):
-		raw_data.append(read_4ch_trigger(f'{bureaucrat.raw_data_dir_path}/{fname}'))
-	
-	x_positions = [data['position'][0] for data in raw_data]
-	x_positions = sorted(list(set(x_positions))) # Remove duplicate elements.
-	y_positions = [data['position'][1] for data in raw_data]
-	y_positions = sorted(list(set(y_positions))) # Remove duplicate elements.
-	
-	print('Calculating amplitudes...')
-	amplitudes = {
-		'CH1': np.zeros((len(x_positions),len(y_positions))),
-		'CH2': np.zeros((len(x_positions),len(y_positions))),
-		'CH3': np.zeros((len(x_positions),len(y_positions))),
-		'CH4': np.zeros((len(x_positions),len(y_positions))),
-	}
-	for data in raw_data:
-		nx = x_positions.index(data['position'][0])
-		ny = y_positions.index(data['position'][1])
-		for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
-			signal = LGADSignal(
-				time = data[ch]['time'],
-				samples = data[ch]['volt']*-1,
-			)
-			amplitudes[ch][nx,ny] = signal.amplitude
-	
-	print('Saving parsed data to files...')
-	np.savetxt(f'{bureaucrat.processed_data_dir_path}/x_positions.txt', x_positions)
-	np.savetxt(f'{bureaucrat.processed_data_dir_path}/y_positions.txt', y_positions)
-	for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
-		np.savetxt(f'{bureaucrat.processed_data_dir_path}/amplitudes_{ch}.txt', amplitudes[ch])
-
-def plot_amplitudes():
-	x_positions = np.genfromtxt(f'{bureaucrat.processed_data_dir_path}/x_positions.txt')
-	y_positions = np.genfromtxt(f'{bureaucrat.processed_data_dir_path}/y_positions.txt')
-	amplitudes = {}
-	for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
-		amplitudes[ch] = np.genfromtxt(f'{bureaucrat.processed_data_dir_path}/amplitudes_{ch}.txt').transpose()
-	xx, yy = np.meshgrid(x_positions, y_positions)
-	for ch in ['CH1', 'CH2', 'CH3', 'CH4']:
-		fig = mpl.manager.new(
-			title = f'{ch} amplitude colormap',
-			subtitle = f'Measurement {bureaucrat.measurement_name}',
-			xlabel = '$x$ position (mm)',
-			ylabel = '$y$ position (mm)',
-			aspect = 'equal',
-			package = 'plotly',
-		)
-		fig.colormap(
-			x = xx*1e3,
-			y = yy*1e3,
-			z = amplitudes[ch],
-		)
-	mpl.manager.save_all(mkdir=f'{bureaucrat.processed_data_dir_path}/amplitude colormaps plots')
-	mpl.manager.show()
 
 ############################################################
 
 if __name__ == '__main__':
-	print('Measuring...')
-	measure()
-	print('Parsing amplitudes...')
-	parse_amplitudes()
-	print('Plotting amplitudes...')
-	plot_amplitudes()
-	print(f'Data was saved in {bureaucrat.measurement_path}')
+	import sys
+	sys.path.append(str(Path('C:/Users/tct_cms/Desktop/phd-scripts/ac-lgad')))
+	from parse_raw_data import script_core as parse_raw_data
+	from plot_parsed_data import script_core as plot_parsed_data
+	
+	measurement_names = input('Measurement name? ').replace(' ', '_')
+	while True:
+		print('Starting a new measurement...')
+		script_core(
+			measurement_name = measurement_names,
+			x_start = 21.14e-3,
+			x_end = 21.32e-3,
+			y_start = 37.28e-3,
+			y_end = 37.51e-3,
+			n_steps = 99,
+			z_focus = 54.5e-3,
+			n_average_triggers = 1,
+		)
+		print('Parsing raw data...')
+		parse_raw_data(
+			str(sorted(list(Path('C:/Users/tct_cms/Desktop/TCT_measurements_data').iterdir()))[-1]),
+		)
+		print('Plotting parsed data...')
+		plot_parsed_data(
+			str(sorted(list(Path('C:/Users/tct_cms/Desktop/TCT_measurements_data').iterdir()))[-1]),
+		)
