7d6d3185a1be0641c97098dac2a1e26638dbe7b7
Matias Senger <m.senger@hotmail.com>
2021-10-05 20:00:57 +0200

    Delete delete_me.py

--- PyticularsTCT/laser_api/delete_me.py
+++
@@ -1,8 +0,0 @@
-from subprocess import check_output
-from pathlib import Path
-
-PROGRAM_PATH = str(Path('PaLaser-C_API_V1.0/PaLaser.exe'))
-
-result = check_output(PROGRAM_PATH + ' -s').decode()
-
-print(result)
